a7c7cdd4a55deb2201bdead2ad510c3793bbdf8a
Hannah Bradley <hannahthesailor@yahoo.com>
2024-03-18 22:18:41 -0400

    python-p3-oo-counting-sentences-lab.md

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+
+  def __init__(self, value=''):
+      self.value = value
+  
+  def get_value(self):
+      return self.get_value
+  
+  def set_value(self, value):
+      if not isinstance(value, str):
+        print("The value must be a string.")
+      else:
+         self._value = value
+
+  value = property(get_value, set_value)
+  
+  def is_sentence(self):
+    if self._value.endswith('.'):
+        return True
+    else:
+        return False
+    
+  def is_question(self):
+    if self._value.endswith('?'):
+        return True
+    else:
+        return False
+    
+  def is_exclamation(self):
+    if self._value.endswith('!'):
+        return True
+    else:
+        return False
+    
+  def count_sentences(self):
+        # Split the string by '.', '?', and '!'
+        sentences = filter(None, self._value.split('.'))
+        sentences = sum(map(lambda x: x.split('!'), sentences), [])
+        sentences = sum(map(lambda x: x.split('?'), sentences), [])
+        # Count the non-empty strings
+        return sum(1 for sentence in sentences if sentence.strip())
+      
